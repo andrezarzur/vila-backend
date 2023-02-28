477828f71d33f8c3b91469b853833be998843224
andrezarzur <andrezarzur@hotmail.com>
2023-02-28 18:49:13 -0300

    adjust

--- modules/socket.js
+++ modules/socket.js
@@ -2,6 +2,7 @@ let users = []
 
 module.exports = function (io) {
     io.on('connection', (socket) => {
+        
         console.log('The user ' + socket.id + ' connected');
     });
 };
--- socket/User.js
+++ socket/User.js
@@ -1,14 +1,29 @@
 class User {
 
+    users = [];
     socket = undefined;
     name = undefined;
   
-    constructor(socket) {
-      this.socket = socket;
+    constructor(socket, name) {
+        this.socket = socket;
+        this.name = name;
     }
   
     setName(name) {
-      this.name = name;
+        this.name = name;
+    }
+
+    addUser(user) {
+        this.list.push(user);
+    } 
+    
+    removeUser(id) {
+        this.list = this.list.filter(u => u.socket.id !== id);
+    } 
+    
+    findUser(id) {
+        var user = this.list.filter(u => u.socket.id === id);
+        return user[0];
     }
   
   }
